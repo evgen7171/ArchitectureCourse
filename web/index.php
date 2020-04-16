88193dec5d81d202b574cb331e4083b3cad606ea
evgen7171 <figaro1428@mail.ru>
2020-04-16 14:52:46 +0300

    'FrontController'

--- web/index.php
+++ web/index.php
@@ -8,9 +8,17 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .
     'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $request = Request::createFromGlobals();
+
+//паттерн Front Controller релизован через подключаемый класс Request - для обработки всех запросов.
+//формируется сам запрос с помощью метода createFromGlobals
+
 $containerBuilder = new ContainerBuilder();
 
 Registry::addContainer($containerBuilder);
 
 $response = (new Kernel($containerBuilder))->handle($request);
+
+//запрос обрабатывается с поомщью метода handle "ядра" приложения,
+// помещенного в контайнер - передается параметр containerBuilder
+
 $response->send();
